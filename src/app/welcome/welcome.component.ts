eb0710d1e50ed146652f0ad7aad0d404589ad76e
antoniomancera <antoniomanuelmancerahevilla@gmail.com>
2020-12-12 14:13:14 +0100

    Sigo poniéndolo bien

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Datafilms } from '../interfaces/datafilms';
-import { Book } from '../interfaces/date-books';
+import { Book, DateBooks } from '../interfaces/date-books';
 import { GoodreadsService} from '../services/goodreads.service';
 @Component({
   selector: 'app-welcome',
@@ -9,10 +9,12 @@ import { GoodreadsService} from '../services/goodreads.service';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-books:Book[];
+books:Book[]=[];
 datafilm:Datafilms;
 titulo:string;
+databooks:DateBooks;
 
+rings:string="rings";
   constructor(private GoodreadsService:GoodreadsService) { }
 
   ngOnInit() {}
@@ -21,7 +23,15 @@ titulo:string;
     this.GoodreadsService.getBooks(search).subscribe(data=>{
       this.books=data.items;
       for(let book of this.books){
-        book.films=this.getFilmsrelatedBook(book.volumeInfo.title);
+        this.GoodreadsService.getFilms(book.volumeInfo.title).subscribe(data=>{
+         
+          book.films=data;
+        
+        },
+        error=>console.log(error),
+        ()=>console.log("peliculas posiblemente relacionadas con",book.volumeInfo.title,book)
+        )
+        //console.log('las películas relacionadas con el libro son:',book.films);
       }
     },
       error=>console.log(error),
@@ -32,7 +42,32 @@ titulo:string;
     this.GoodreadsService.getFilms(search).subscribe(data=>{
       this.datafilm=data;
     })
+    
     return this.datafilm;
+    
   }
+  getLibros(libro:string){
+    this.GoodreadsService.getBooks3(libro).subscribe(data=>{
+      this.databooks=data;
+      this.books=this.databooks.items;
+  
+      for(let book of this.books){
+  
+        this.GoodreadsService.getFilms(book.volumeInfo.title).subscribe(data=>{
+          console.log('resultados de getfilms',data);
+          book.films=data;
+          console.log(book.volumeInfo.title,book.films);
+        },
+        error=>console.log(error),
+        ()=>console.log("peliculas posiblemente relacionadas con",book)
+        )
+        console.log('prueba',this.books[0]);
+  
+      }  
+    })
+  
+  
+      }
+  
 
 }
